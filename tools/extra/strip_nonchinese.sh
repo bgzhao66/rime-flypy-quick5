2b42231ffde137d28fdfede708c1e07766639cd6
benguang.zhao <benguang.zhao@outlook.com>
2025-10-01 07:18:00 +0800

    Update extra/strip_nonchinese.sh

--- tools/extra/strip_nonchinese.sh
+++ tools/extra/strip_nonchinese.sh
@@ -2,4 +2,7 @@
 #
 # Strip lines containing non-Chinese characters from a text file.
 #
-perl -pi -e '$_ = "" if /[0-9a-zA-Z]/' $1
+perl -pi -e '$_ = "" if /^\S*[0-9a-zA-Z]/' $1
+perl -pi -e '$_ = "" if /哈哈哈/' $1
+perl -pi -e 's/·//g' $1
+perl -pi -e '$_ = "" if /^\t/' $1
